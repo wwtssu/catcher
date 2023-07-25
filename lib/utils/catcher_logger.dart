0acc2c830f03d1797341ae12ca8e0b84121e06ce
wwts <im@wwts.me>
2023-07-25 20:29:24 +0800

    CatcherLogger only log catcher logs

--- lib/utils/catcher_logger.dart
+++ lib/utils/catcher_logger.dart
@@ -2,17 +2,20 @@ import 'package:logging/logging.dart';
 
 ///Class used to provide logger for Catcher.
 class CatcherLogger {
-  final Logger _logger = Logger("Catcher");
+  static String catcherLoggerName = "Catcher";
+  final Logger _logger = Logger(catcherLoggerName);
 
   ///Setup logger configuration.
   void setup() {
     Logger.root.level = Level.ALL;
     Logger.root.onRecord.listen(
       (LogRecord rec) {
-        // ignore: avoid_print
-        print(
-          '[${rec.time} | ${rec.loggerName} | ${rec.level.name}] ${rec.message}',
-        );
+        if(rec.loggerName == catcherLoggerName){
+          // ignore: avoid_print
+          print(
+            '[${rec.time} | ${rec.loggerName} | ${rec.level.name}] ${rec.message}',
+          );
+        }
       },
     );
   }
